2c52a7c9a53d279a6da6af8033105b633485c77c
AMITH RATHOD <amithrathod247@gmail.com>
2026-03-14 20:53:15 +0530

    folder structure added!

---
+++ template.sh
@@ -0,0 +1,16 @@
+#Creating the directory structure for the project
+mkdir -p src
+mkdir -p research
+
+#Creating files for the project
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trial.ipynb
+touch requirements.txt
+
+
+echo "Directory and files created successfully!"
